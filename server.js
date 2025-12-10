5e6f92bcd59055a07a6ff828026b2a3cade69c20
alokprakash7766 <your_email@domain.com>
2025-12-10 15:37:57 +0530

    vercel

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
+// CORS
 app.use(cors({
-  origin: "https://mern-auth-ruddy.vercel.app",
+  origin: ["https://mern-auth-ruddy.vercel.app", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
@@ -17,15 +18,18 @@ app.use(cors({
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+// Connect DB
 connectDB();
 
+// Routes
 app.use("/api/auth", authRoutes);
-
 
 app.get("/", (req, res) => {
   res.send({ message: "Backend is working fine!" });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// Listen
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+export default app;
