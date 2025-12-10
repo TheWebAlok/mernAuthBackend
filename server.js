39acc1296a10c5916bd302b28c04c877618f4f44
alokprakash7766 <your_email@domain.com>
2025-12-10 16:26:08 +0530

    api

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ dotenv.config();
 const app = express();
 
 // CORS
-app.use(cors({
-  origin: ["https://mern-auth-ruddy.vercel.app", "http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://mern-auth-ruddy.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
@@ -28,8 +30,13 @@ app.get("/", (req, res) => {
   res.send({ message: "Backend is working fine!" });
 });
 
-// Listen
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// ---------------------------------------
+// LOCAL run ONLY (Not on Vercel)
+// ---------------------------------------
+if (process.env.VERCEL !== "1") {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+}
 
+// Vercel needs default export
 export default app;
