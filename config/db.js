1b289c7f7c48238640f95521fe52ffaa010ec6ab
alokprakash7766 <your_email@domain.com>
2025-12-10 16:36:13 +0530

    api

--- config/db.js
+++ config/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_Url);
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
-    console.log(err);
+    console.log("DB Error:", err.message);
   }
 };
 
